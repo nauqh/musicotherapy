7aa6cb77a1c2defa69e36352c2fdc4e3562e1ad1
Nauqh <hodominhquan.work@gmail.com>
2024-02-02 14:30:01 +0700

    refactor (App.tsx): Add auto scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import Fetch from "./pages/Fetch";
 import Input from "./pages/Input";
 import Diagnose from "./pages/Diagnose";
 
 function App() {
+	useEffect(() => {
+		window.history.scrollRestoration = "manual";
+	}, []);
+
 	return (
 		<div>
 			<BrowserRouter>
